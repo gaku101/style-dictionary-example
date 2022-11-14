f5fd3f7a5ee3436c5729b884223e40ae6e2fd3f1
takehiro <takehiro-r133@hotmail.co.jp>
2022-11-14 15:58:13 +0900

    first commit

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Sun, 13 Nov 2022 09:15:01 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
